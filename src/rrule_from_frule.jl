7ca0048b94bf8a61d23cae9d071a9bd708cfb582
Niklas Schmitz <niklas.f.schmitz@gmail.com>
2021-10-05 19:44:12 +0000

    add support for varargs

--- src/rrule_from_frule.jl
+++ src/rrule_from_frule.jl
@@ -15,24 +15,20 @@ Further Reading
 """
 macro rrule_from_frule(signature_expression)
     @assert Meta.isexpr(signature_expression, :call)
-    @assert length(signature_expression.args) == 2 "Only single-argument functions are implemented."
-    # TODO add support for multiple arguments, varargs, kwargs
-
     f = signature_expression.args[1]
-    arg = signature_expression.args[2]
-    @assert Meta.isexpr(arg, :(::), 2)
-
-    return rrule_from_frule_expr(__source__, f, arg)
+    args = signature_expression.args[2:end]
+    @assert all(Meta.isexpr.(args, :(::), 2))
+    return rrule_from_frule_expr(__source__, f, args)
 end
 
-function rrule_from_frule_expr(__source__, f, arg)
+function rrule_from_frule_expr(__source__, f, args)
     f_instance_name  = gensym(Symbol(:instance_, Symbol(f)))
     return quote
-        function ChainRulesCore.rrule(config::RuleConfig{>:HasReverseMode}, $f_instance_name::Core.Typeof($(esc(f))), $arg)
+        function ChainRulesCore.rrule(config::RuleConfig{>:HasReverseMode}, $f_instance_name::Core.Typeof($(esc(f))), $(args...))
             $(__source__)
-            pushforward(Δfarg...) = frule(Δfarg, $f_instance_name, $arg)[2]
-            _, back = rrule_via_ad(config, pushforward, $f_instance_name, $arg)
-            y = $f_instance_name($arg) # TODO optimize away redundant primal computation
+            pushforward(Δfarg...) = frule(Δfarg, $f_instance_name, $(args...))[2]
+            _, back = rrule_via_ad(config, pushforward, $f_instance_name, $(args...))
+            y = $f_instance_name($(args...)) # TODO optimize away redundant primal computation
             f_pullback(Δy) = back(Δy)[2:end]
             return y, f_pullback
         end
--- test/rrule_from_frule.jl
+++ test/rrule_from_frule.jl
@@ -1,21 +1,47 @@
 @testset "rrule_from_frule" begin
-    function f(x)
-        a = sin.(x)
-        b = sum(a)
-        c = b * a
-        return c
-    end
+    @testset "single argument" begin
+        function f(x)
+            a = sin.(x)
+            b = sum(a)
+            c = b * a
+            return c
+        end
+        
+        function ChainRulesCore.frule((Δself, Δx), ::typeof(f), x)
+            a, ȧ = sin.(x), cos.(x) .* Δx 
+            b, ḃ = sum(a), sum(ȧ)
+            c, ċ = b * a, ḃ * a + b * ȧ
+            return c, ċ
+        end
+        
+        x = rand(3)
+        test_frule(f, x)
     
-    function ChainRulesCore.frule((Δself, Δx), ::typeof(f), x)
-        a, ȧ = sin.(x), cos.(x) .* Δx 
-        b, ḃ = sum(a), sum(ȧ)
-        c, ċ = b * a, ḃ * a + b * ȧ
-        return c, ċ
+        @rrule_from_frule f(x::AbstractArray{<:Real})
+        test_rrule(Zygote.ZygoteRuleConfig(), f, x; check_inferred=false)    
     end
+    @testset "multiple arguments" begin
+        function f(x, z)
+            a = x + z
+            b = sin.(a)
+            c = sum(b)
+            d = c * b
+            return d
+        end
+        
+        function ChainRulesCore.frule((Δself, Δx, Δz), ::typeof(f), x, z)
+            a, ȧ = x + z, Δx + Δz
+            b, ḃ = sin.(a), cos.(a) .* ȧ
+            c, ċ = sum(b), sum(ḃ)
+            d, ḋ = c * b, ċ * b + c * ḃ
+            return d, ḋ
+        end
+        
+        x = rand(3)
+        z = rand(3)
+        test_frule(f, x, z)
     
-    x = rand(3)
-    test_frule(f, x)
-
-    @rrule_from_frule f(x::AbstractArray{<:Real})
-    test_rrule(Zygote.ZygoteRuleConfig(), f, x; check_inferred=false)
+        @rrule_from_frule f(x::AbstractArray{<:Real}, z::AbstractArray{<:Real})
+        test_rrule(Zygote.ZygoteRuleConfig(), f, x, z; check_inferred=false)    
+    end
 end
